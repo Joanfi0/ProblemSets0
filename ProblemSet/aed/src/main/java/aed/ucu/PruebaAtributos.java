767661bef0b9247273c77a6d5a8851f69258cb8a
joanfi0 <jorgeandresfs01@gmail.com>
2026-08-05 15:12:11 -0300

    Ejercicio 1

---
+++ ProblemSet/aed/src/main/java/aed/ucu/PruebaAtributos.java
@@ -0,0 +1,35 @@
+package aed.ucu;
+
+public class PruebaAtributos {
+    int numero;
+    boolean activo;
+    double precio;
+    char letra;
+    String texto;
+
+    public static void main(String[] args) {
+        PruebaAtributos prueba = new PruebaAtributos();
+
+        System.out.println("int " + prueba.numero);
+        System.out.println("boolean: " + prueba.activo);
+        System.out.println("double: " + prueba.precio);
+        System.out.println("char: [" + prueba.letra + "]");
+        System.out.println("String: " + prueba.texto);
+
+        int edad = 20;
+        int cantidadTotal = 5;
+        int _contador = 1;
+        int numero2 = 2;
+
+        System.out.println("Edad: " + edad);
+        System.out.println("Cantidad total: " + cantidadTotal);
+        System.out.println("Contador: " + _contador);
+        System.out.println("Número 2: " + numero2);
+
+
+        int variableLocal;
+
+        variableLocal = 10;
+        System.out.println("Variable local: " + variableLocal);
+    }
+}
